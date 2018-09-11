a645c45b58e4a21e006b25a6a219e92955973695
Andre Abrantes <andredossantosabrantes@gmail.com>
2018-09-11 15:05:47 -0300

    Adwords MVP

---
+++ CHANGELOG.md
@@ -0,0 +1,30 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [0.3.0] - 2018-6-11
+
+### Added
+- Support for internal and external link routes.
+- Internationalization to the schema.
+
+## [0.2.0] - 2018-5-30
+
+### Changed
+- Hide menu on mobile mode.
+
+## [0.1.1] - 2018-05-07
+
+### Fixed 
+- Fix format text of left item. 
+
+## [0.1.0] - 2018-05-05
+
+### Added
+ - Initial files and logic created.
+
---
+++ react/index.js
@@ -0,0 +1,61 @@
+import { Component } from 'react'
+
+import { Helmet } from 'render'
+
+import { PixelAppInterface } from 'vtex.store/PixelContext'
+
+const APP_LOCATOR = 'vtex.adwords'
+
+class Adwords extends Component {
+  constructor(props) {
+    super(props)
+    this.props.subscribe(this)
+  }
+  
+  gtag(data) {
+    dataLayer.push(data)
+  }
+  
+  get adwordsID() {
+    const { adwordsID } = this.context.getSettings(APP_LOCATOR) || {} 
+    if (!adwordsID) {
+      console.warn("Warning: The Google Adwords ID isn't set on your environment.")
+    }
+    return adwordsID 
+  }
+
+  productView = (event) => {
+    console.log(">>> Adwords >>> Product view: ", event)
+    const skuId = event.products[0].id
+    this.gtag({
+      event: 'page_view',
+      send_to: this.adwordsID,
+      ecomm_prodid: skuId
+    })
+  }
+
+  departmentView = (event) => {
+    console.log(">>> Adwords >>> Department View: ", event)
+  }
+
+  componentDidMount() {
+    window.dataLayer = window.dataLayer || []
+    this.gtag({js: new Date()})
+    this.gtag({config: this.adwordsID})
+  }
+
+  render() {
+    return (
+      <Helmet>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${this.adwordsID}`}></script>
+      </Helmet>
+    )
+  }
+}
+
+Adwords.contextTypes = {
+  getSettings: PropTypes.func,
+  context: PropTypes.object
+}
+
+export default PixelAppInterface(Adwords)
