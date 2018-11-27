d125ae09c2f02d2442d331e5e02932cb383b6eb3
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-11-27 09:27:46 -0300

    Migrate to new pixel architecture

--- react/index.tsx
+++ react/index.tsx
@@ -1,84 +1,33 @@
-import PropTypes from 'prop-types'
-import React, { Component } from 'react'
-import { Helmet } from 'render'
-import { Pixel } from 'vtex.store/PixelContext'
+const adwordsId = window.__SETTINGS__.adwordsId
 
-const APP_LOCATOR = 'vtex.google-adwords'
-
-interface Props {
-  subscribe: (s: any) => () => void
+if (!adwordsId) {
+  throw new Error('Warning: No Google Adwords ID is defined. To setup the app, go to your admin.')
 }
 
-/**
- * Component that encapsulate the communication to
- * the GoogleAdwords and listen for events comming
- * from the store through the Pixel HOC.
- * It injects the gtag script to the HTML Head.
- */
-class GoogleAdwords extends Component<Props> {
-  public static contextTypes = {
-    /** Function to bind the APP Settings. */
-    getSettings: PropTypes.func,
-  }
+const script = document.createElement('script')
 
-  private unsubscribe: () => void
+script.src = `https://www.googletagmanager.com/gtag/js?id=${adwordsId}`
+script.async = true
 
-  constructor(props: Props) {
-    super(props)
-    this.unsubscribe = this.props.subscribe(this)
-  }
+document.head!.prepend(script)
 
-  public gtag(data: any) {
-    window.dataLayer.push(data)
-  }
+window.dataLayer = window.dataLayer || []
 
-  get adwordsID() {
-    const { adwordsID } = this.context.getSettings(APP_LOCATOR) || { adwordsID: undefined }
+window.addEventListener('message', e => {
+  switch (e.data.eventName) {
+    case 'vtex:productView': {
+      const { product } = e.data
+      const skuId = product.id
 
-    return adwordsID
-  }
-
-  public productView = (event: any) => {
-    const { products } = event
-    let skuId = null
-    if (products && products.length > 0) {
-      skuId = products[0].id
+      window.dataLayer.push({
+        ecomm_prodid: skuId,
+        event: 'view_item',
+        send_to: adwordsId,
+      })
+      return
     }
-    this.gtag({
-      ecomm_prodid: skuId,
-      event: 'view_item',
-      send_to: this.adwordsID,
-    })
-  }
-
-  public componentDidMount() {
-    if (!this.adwordsID) {
-      console.warn('Warning: The Google Adwords ID isn\'t set on your environment.')
-    }
-
-    window.dataLayer = window.dataLayer || []
-    this.gtag({js: new Date()})
-    this.gtag({config: this.adwordsID})
-  }
-
-  public componentWillUnmount() {
-    if (this.unsubscribe) {
-      this.unsubscribe()
+    default: {
+      return
     }
   }
-
-  public shouldComponentUpdate() {
-    // should only be rendered once
-    return false
-  }
-
-  public render() {
-    return (
-      <Helmet>
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${this.adwordsID}`} />
-      </Helmet>
-    )
-  }
-}
-
-export default Pixel(GoogleAdwords)
+})
