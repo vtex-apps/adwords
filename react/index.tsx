69abe4f2b1ac6283763099a9ee3aecb071875c62
Pedro Frayha <pedro.frayha@vtex.com>
2019-05-10 18:41:07 -0300

    Includes orderPlaced event manipulation

--- react/index.tsx
+++ react/index.tsx
@@ -21,18 +21,34 @@ gtag('js', new Date())
 gtag('config', adwordsId)
 
 window.addEventListener('message', e => {
+  debugger
   switch (e.data.eventName) {
     case 'vtex:productView': {
       const { product } = e.data
-      const skuId = product.id
-
-      gtag({
+      const skuId = product.id || product.productId
+      const productDetails = {
         ecomm_prodid: skuId,
         event: 'view_item',
         send_to: adwordsId,
-      })
+      }
+      console.log(productDetails)
+      gtag(productDetails)
       return
     }
+
+    case 'vtex:orderPlaced': {
+      const conversionObject = {
+        event: 'conversion',
+        send_to: adwordsId,
+        value: e.data.transactionTotal || 0,
+        currency: e.data.transactionCurrency || 'BRL',
+        transaction_id: e.data.transactionId || '',
+      }
+      console.log(conversionObject)
+      gtag(conversionObject)
+      return
+    }
+
     default: {
       return
     }
